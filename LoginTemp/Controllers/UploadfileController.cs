1b78502949f21b53ec9c04199516e6abb8aedfe4
AndersUOlsson <olsson.anders@protonmail.com>
2018-11-12 17:04:51 +0100

    Upload

---
+++ LoginTemp/Controllers/UploadfileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace LoginTemp.Controllers
+{
+    public class UploadfileController : Controller
+    {
+        // GET: Uploadfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Uploadfile
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(HttpPostedFileBase namefile)
+        {
+            // TODO: SIKJSJDSf
+            try
+            {
+                if(namefile.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(namefile.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    namefile.SaveAs(path);
+                    ViewBag.success = "Uploaded";
+                }
+                else
+                {
+                    ViewBag.error = "You need select file";
+                }
+            }
+            catch (Exception e)
+            {
+                ViewBag.error = "Error!" + e;
+            }
+            return View();
+        }
+    }
+}
---
+++ LoginTemp/Models/UserAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginTemp.Models
+{
+    public class UserAccount
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password",ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
